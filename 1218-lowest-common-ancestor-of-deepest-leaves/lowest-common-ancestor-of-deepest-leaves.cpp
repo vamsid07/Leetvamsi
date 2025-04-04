b87cecf3b8e190669541401be5e68beb5205cffb
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2025-04-04 09:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (39.60%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) {
+            return {NULL, 0};
+        }
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        }
+        if (left.second < right.second) {
+            return {right.first, right.second + 1};
+        }
+        return {root, left.second + 1};
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) { 
+        return dfs(root).first; 
+    }
+};
