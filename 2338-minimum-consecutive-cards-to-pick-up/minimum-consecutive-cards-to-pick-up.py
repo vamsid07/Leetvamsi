426801309bab9888e804b9b5302197d0e4b9322a
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2025-06-11 20:32:45 +0530

    Time: 233 ms (5.00%) | Memory: 42.3 MB (17.20%) - LeetSync

---
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        d = {}
+        d = {i:float('inf') for i in set(cards)}
+        n = len(cards)
+        if len(set(cards)) == len(cards) : 
+            return -1 
+        ans = float('inf')
+        for i in range(n): 
+            if i-d[cards[i]] +1 > 0 :
+                ans = min(ans,i - d[cards[i]] + 1)
+                #print(i-d[cards[i]] +1)
+            d[cards[i]] = i 
+        return ans
