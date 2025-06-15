de81edadaf797c743f9725b256a619594fdfe8fa
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2025-06-15 08:56:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (38.64%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -1,26 +1,30 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        d1 = defaultdict(list)  # col: list of (row, val)
-        d2 = defaultdict(int)   # val: col
+        d1 = defaultdict(list) 
+        d2 = defaultdict(int)   
         q = deque()
         d2[root.val] = 0 
-        q.append((root, 0))  # (node, row)
-        d1[0].append((0, root.val))  # col = 0, row = 0
-
+        q.append((root, 0))
+        d1[0].append((0, root.val))
         while q:
             node, row = q.popleft()
             col = d2[node.val]
             if node.left:
                 d2[node.left.val] = col - 1
-                d1[col - 1].append((row + 1, node.left.val))
+                d1[col-1].append((row + 1, node.left.val))
                 q.append((node.left, row + 1))
             if node.right:
                 d2[node.right.val] = col + 1
                 d1[col + 1].append((row + 1, node.right.val))
                 q.append((node.right, row + 1))
-
         ans = []
-        for key in sorted(d1.keys()):
-            level = sorted(d1[key], key=lambda x: (x[0], x[1]))
-            ans.append([val for row, val in level])
+        keys = list(d1.keys())
+        keys.sort()
+        for key in keys:
+            level = d1[key]
+            level.sort()
+            temp = []
+            for i in level:
+                temp.append(i[1])
+            ans.append(temp)
         return ans
