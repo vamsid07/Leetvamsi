2f08d3ed70b91cccac19f3bf4b005a9338635aaf
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2025-07-13 11:59:07 +0530

    Time: 151 ms (92.23%) | Memory: 64.3 MB (93.01%) - LeetSync

---
+++ 3881-minimize-maximum-component-cost/minimize-maximum-component-cost.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]], k: int) -> int:
+        if k >= n:
+            return 0
+        edges.sort(key=lambda x: x[2])
+        p = list(range(n))
+        r = [0] * n
+        def find(x):
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+        def union(x, y):
+            a, b = find(x),find(y)
+            if a == b:
+                return False
+            if r[a] < r[b]:
+                a,b = b,a
+            p[b] = a
+            if r[a] == r[b]:
+                r[a] += 1
+            return True
+        count = n
+        for u, v, w in edges:
+            if union(u, v):
+                count -= 1
+                if count == k:
+                    return w
+        return 0
