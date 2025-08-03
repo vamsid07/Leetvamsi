a83c64891571432b86e6d102a45a480a69a0300b
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2025-08-03 17:27:32 +0530

    Time: 4477 ms (18.59%) | Memory: 779.5 MB (7.65%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -1,29 +1,21 @@
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
-        mod=10**9+7
         n = len(grid)
         m = len(grid[0])
+        MOD = 10**9 + 7
         memo = {}
-        
-
-        def solve(i, j, curr_sum):
-            if i == n - 1 and j == m - 1:
-                if (grid[i][j] + curr_sum) % k == 0:
-                    return 1
-                else:
-                    return 0
-
-            if i >= n or j >= m:
+        def go(i,j,tot) :  
+            if i == n - 1 and j == m - 1 : 
+                if (grid[i][j] + tot) % k == 0 : 
+                    return 1 
+                else :
+                    return 0 
+            if i >= n or j >= m : 
                 return 0
-
-            if (i, j, curr_sum) in memo:
-                return memo[(i, j, curr_sum)]
-
-            down = solve(i + 1, j, (curr_sum + grid[i][j]) % k)
-
-            right = solve(i, j + 1, (curr_sum + grid[i][j]) % k)
-            memo[(i, j, curr_sum)] = (down + right) % mod
-
-            return memo[(i, j, curr_sum)]
-
-        return solve(0, 0, 0)
+            if (i,j,tot) in memo : 
+                return memo[(i,j,tot)]
+            op2 = go(i+1,j,(tot+grid[i][j])%k)
+            op1 = go(i,j+1,(tot+grid[i][j])%k)
+            memo[(i,j,tot)] = (op1 + op2) % MOD
+            return memo[(i,j,tot)]
+        return go(0,0,0)
