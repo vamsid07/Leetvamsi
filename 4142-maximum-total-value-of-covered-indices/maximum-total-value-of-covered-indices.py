155b699ad3dc7f986389d214cd9dd98973736a90
Vamsidhar Venkatarman <vamsidhar.venkataraman@gmail.com>
2026-06-06 23:45:58 +0530

    Time: 1732 ms (5.10%) | Memory: 607.1 MB (5.01%) - LeetSync

---
+++ 4142-maximum-total-value-of-covered-indices/maximum-total-value-of-covered-indices.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTotal(self, nums: List[int], s: str) -> int:
+        n = len(nums)
+        INF = 10**20
+        @cache
+        def go(curr,prev) : 
+            if curr >= n : 
+                return 0
+            op1 = go(curr+1,s[curr])
+            op2 = -INF
+            if s[curr] == '1' : 
+                t1 = nums[curr] + go(curr+1,s[curr])
+                t2 = -INF
+                if prev == '0' : 
+                    t2 = nums[curr-1] + go(curr+1,'0')
+                op2 = max(t1,t2)
+            return max(op1,op2)
+        ans = 0 
+        if s[0] == '1' : 
+            ans = nums[0]
+        return ans + go(1,s[0])
